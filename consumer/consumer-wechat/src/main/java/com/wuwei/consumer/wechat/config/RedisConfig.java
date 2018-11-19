299b54199866333db952dbc49b29c728283273f7
735626035@qq.com <735626035@qq.com>
2018-11-19 16:29:25 +0800

    gitignorea

--- consumer/consumer-wechat/src/main/java/com/wuwei/consumer/wechat/config/RedisConfig.java
+++ consumer/consumer-wechat/src/main/java/com/wuwei/consumer/wechat/config/RedisConfig.java
@@ -1,9 +1,7 @@
 package com.wuwei.consumer.wechat.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
@@ -37,11 +35,6 @@ public class RedisConfig {
 
     @Value("${spring.redis.timeout}")
     private int timeout;
-
-//    @Bean
-//    public JedisConnectionFactory connectionFactory() {
-//        return new JedisConnectionFactory();
-//    }
 
     public String getHost() {
         return host;
