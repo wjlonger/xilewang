a3aa936d29c8b7a19a5b3e013bf503f1d5dc6f9c
735626035@qq.com <735626035@qq.com>
2018-11-22 10:04:05 +0800

    gitignorea

--- provider/provider-jd/src/main/java/com/wuwei/provider/jd/config/JdCpsConfig.java
+++ provider/provider-jd/src/main/java/com/wuwei/provider/jd/config/JdCpsConfig.java
@@ -2,6 +2,7 @@ package com.wuwei.provider.jd.config;
 
 import com.jd.open.api.sdk.DefaultJdClient;
 import com.jd.open.api.sdk.JdClient;
+import com.wuwei.base.utils.AES;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,38 +26,6 @@ public class JdCpsConfig {
 
     @Bean("jdClient")
     public JdClient jdClient(){
-        return new DefaultJdClient(serverUrl,accessToken,appKey,appSecret);
-    }
-
-    public String getServerUrl() {
-        return serverUrl;
-    }
-
-    public void setServerUrl(String serverUrl) {
-        this.serverUrl = serverUrl;
-    }
-
-    public String getAppKey() {
-        return appKey;
-    }
-
-    public void setAppKey(String appKey) {
-        this.appKey = appKey;
-    }
-
-    public String getAppSecret() {
-        return appSecret;
-    }
-
-    public void setAppSecret(String appSecret) {
-        this.appSecret = appSecret;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
+        return new DefaultJdClient(serverUrl,accessToken,AES.AESDncode(appKey),AES.AESDncode(appSecret));
     }
 }
