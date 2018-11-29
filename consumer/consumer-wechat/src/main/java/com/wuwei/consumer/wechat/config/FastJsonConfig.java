2a0efeca61fb7880d29f2ba0b782897eb3034669
735626035@qq.com <735626035@qq.com>
2018-11-29 12:19:03 +0800

    gitignorea

--- consumer/consumer-wechat/src/main/java/com/wuwei/consumer/wechat/config/FastJsonConfig.java
+++ consumer/consumer-wechat/src/main/java/com/wuwei/consumer/wechat/config/FastJsonConfig.java
@@ -14,7 +14,7 @@ public class FastJsonConfig extends com.alibaba.fastjson.support.config.FastJson
         SerializeConfig serializeConfig = SerializeConfig.globalInstance;
         serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
         serializeConfig.put(Long.class,ToStringSerializer.instance);
-        serializeConfig.put(Long.TYPE,ToStringSerializer.instance);
+        serializeConfig.put(Double.class,ToStringSerializer.instance);
         this.setSerializeConfig(serializeConfig);
     }
 
