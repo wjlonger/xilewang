336ecfaed6bca8fb08752b8e3a77b8d2343dcc3c
735626035@qq.com <735626035@qq.com>
2018-12-24 14:46:23 +0800

    gitignorea

--- route/src/main/java/com/wuwei/route/RouteApplication.java
+++ route/src/main/java/com/wuwei/route/RouteApplication.java
@@ -1,11 +1,15 @@
 package com.wuwei.route;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -18,4 +22,21 @@ public class RouteApplication {
         SpringApplication.run(RouteApplication.class,args);
     }
 
+    //下面是2.0的配置，1.x请搜索对应的设置
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+        return tomcat;
+    }
+
+    private Connector createHTTPConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        //同时启用http（8080）、https（8443）两个端口
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(80);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
